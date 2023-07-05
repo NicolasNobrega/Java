4f9aa7dc5e49616f719d58e40cab71a257e9d591
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-07-04 23:58:14 -0300

    Aula 8

---
+++ Aulas POO/Aula8/src/main/java/com/aula/aula8/Aula8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aula.aula8;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Aula8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
