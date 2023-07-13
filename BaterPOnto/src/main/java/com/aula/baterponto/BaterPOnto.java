82a4802ead6e05cdce22dfa0e9236dbaebd6fbf1
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-07-12 23:02:25 -0300

    Bater Ponto Created

---
+++ BaterPOnto/src/main/java/com/aula/baterponto/BaterPOnto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aula.baterponto;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class BaterPOnto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
