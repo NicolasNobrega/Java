a7c1075377c712f5ffcf4435d610f43dfafbb512
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-07-20 20:09:39 -0300

    Game Seed

--- dslist/src/main/java/com/devspring/dslist/DslistApplication.java
+++ dslist/src/main/java/com/devspring/dslist/DslistApplication.java
@@ -1,5 +1,6 @@
 package com.devspring.dslist;
 
+import com.devspring.dslist.entities.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
