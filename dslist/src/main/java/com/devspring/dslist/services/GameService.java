d455d2fc03258724d65e532a053e5a11603482fd
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-07-22 18:11:11 -0300

    GET game by id

--- dslist/src/main/java/com/devspring/dslist/services/GameService.java
+++ dslist/src/main/java/com/devspring/dslist/services/GameService.java
@@ -2,18 +2,37 @@ package com.devspring.dslist.services;
 
 import java.util.List;
 
+import com.devspring.dslist.dto.GameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devspring.dslist.dto.GameMinDTO;
 import com.devspring.dslist.entities.Game;
 import com.devspring.dslist.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
-    
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        try
+        {
+            Game result = gameRepository.findById(id).get();
+            return new GameDTO(result);
+        }
+        catch(NullPointerException e) //CAPTURA DA POSSÍVEL exceção.
+        {
+            //TRATAMENTO DA exceção
+            System.out.println("Id Não existe");
+            Game result = null;
+        }
+        return  null;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
