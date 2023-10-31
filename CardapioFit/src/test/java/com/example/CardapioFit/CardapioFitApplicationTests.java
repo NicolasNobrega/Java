6b1a8d2755e5f31915f54f5e0432227db60e10b0
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-10-30 21:23:09 -0300

    Create CardapioFitApplicationTests.java

---
+++ CardapioFit/src/test/java/com/example/CardapioFit/CardapioFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CardapioFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
