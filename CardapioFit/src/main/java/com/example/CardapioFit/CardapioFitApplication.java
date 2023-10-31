846072a0ce9480cf7ee27060f401820b36c59472
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-10-30 21:22:57 -0300

    Created CardapioFit

---
+++ CardapioFit/src/main/java/com/example/CardapioFit/CardapioFitApplication.java
@@ -0,0 +1,13 @@
+package com.example.CardapioFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioFitApplication.class, args);
+	}
+
+}
